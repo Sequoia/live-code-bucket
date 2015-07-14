8539a794ee73c14c1f882af4e6cf67b77058f539
Sequoia McDowell <sequoia.mcdowell@gmail.com>
2015-07-14 13:12:29 -0700

    added clustering to server

--- server-improved/server.js
+++ server-improved/server.js
@@ -1,10 +1,26 @@
 var http = require('http');
 var requestHandler = require('./requestHandler');
-//SPOILER ALERT!!
-var port = Number(process.argv[2]) || 1337;
+var cluster = require('cluster');
 
-var server = http.createServer(requestHandler);
+if (cluster.isMaster) {
+    
+	console.log('master with pid %d started', process.pid)	
 
-server.listen(port, '127.0.0.1', function() {
-    console.log('Server running at http://127.0.0.1:%d/',port);
-});
+    cluster.fork();
+    cluster.fork();
+    
+    cluster.on('exit', function() {
+        console.log('a worker died...');
+        cluster.fork();
+    });
+    
+} else {
+	var port = Number(process.argv[2]) || 1337;
+
+	var server = http.createServer(requestHandler);
+
+	server.listen(port, '127.0.0.1', function() {
+		console.log('cluster pid %d started', cluster.worker.process.pid)
+	    console.log('Server running at http://127.0.0.1:%d/',port);
+	});
+}
