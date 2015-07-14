525b96d578cab25a94fe4d49f58f14491166c14f
Sequoia McDowell <sequoia.mcdowell@gmail.com>
2015-07-14 16:43:46 -0700

    added user router

--- express-server/app.js
+++ express-server/app.js
@@ -1,7 +1,9 @@
 //app.js
-var myApp  = require('express')();
+var express  = require('express');
 var routes = require('./routes');
 var bodyParser = require('body-parser');
+
+var myApp = express();
 
 //normally you'd save/get users from a database,
 //not an array attached to app :p
@@ -10,11 +12,18 @@ myApp.set('users',[]);
 // Parse POST form data...
 myApp.use( bodyParser.urlencoded({ extended: false }) );
 
+
+	//userRouter.js
+	var userRouter   = express.Router();
+	//user stuff
+	userRouter.post('/',    routes.createUser);
+	userRouter.get( '/:id', routes.getUser);
+	userRouter.get( '/',    routes.userForm);
+	//module.export = userRouter;
+
+myApp.use('/user', userRouter);
+
 myApp.get('/', routes.home);
 myApp.get('/fib', routes.fibonacci);
-//user stuff
-myApp.post('/user', routes.createUser);
-myApp.get('/user/:id', routes.getUser);
-myApp.get('/user', routes.userForm);
 
 myApp.listen(8080);
