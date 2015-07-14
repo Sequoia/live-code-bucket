7e4cb26e516a983da5e392c15aa78e5a86a077d7
Sequoia McDowell <sequoia.mcdowell@gmail.com>
2015-07-14 10:26:17 -0700

    added root & fib routes

--- server-improved/server.js
+++ server-improved/server.js
@@ -1,18 +1,26 @@
 var http = require('http');
+var fib  = require('./lib/fibonacci');
+var url  = require('url');
 //SPOILER ALERT!!
-//take port as first argument or default to 1337
 var port = Number(process.argv[2]) || 1337;
 
 var server = http.createServer(function (req, res) {
+    var parsed = url.parse(req.url,true);
+    console.log('request to ', parsed.pathname, parsed.query);
+    res.writeHead(200, {
+        'Content-Type': 'text/plain' //@TODO probably not right...
+    });
     //if path '/': return "<h1>Hello world</h1><p>welcome to my homepage</p>"
+    if(parsed.pathname === '/'){
+    	res.end("<h1>Hello world</h1><p>welcome to my homepage</p>");
+    }else if(parsed.pathname === '/fib'){
+    	var iterations = Number(parsed.query.iterations);
+    	res.end(JSON.stringify({result: fib(iterations)}));
+    }
     //if the path is '/fib':
     	//get 'iterations' from query string, pass to fib module
     	// domain:port/fib?iterations=10 -->
     	//return result thus: { result: 55 };
-    console.log('request to ', req.url);
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
     res.end('Hello World\n');
     
 });
