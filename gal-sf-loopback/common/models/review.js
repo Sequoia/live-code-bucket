6905a8f61f57f0cd8fb13d18b5b58168b8027cba
Sequoia McDowell <sequoia.mcdowell@gmail.com>
2015-07-15 16:00:06 -0700

    updating models with ACL

--- gal-sf-loopback/common/models/review.js
+++ gal-sf-loopback/common/models/review.js
@@ -1,3 +1,11 @@
 module.exports = function(Review) {
-
+	Review.validate('rating', ratingCheck,{
+		message : "that is not a valid rating!!!!"
+	});
 };
+
+function ratingCheck(errCallback) {
+    if (this.rating > 5 || this.rating < 1) {
+        errCallback();
+    }
+}
