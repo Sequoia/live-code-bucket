e744c698db20e638523e9a13b8d23f61598f6b60
Sequoia McDowell <sequoia.mcdowell@gmail.com>
2015-07-28 15:24:25 -0700

    error + 404 handling

--- seattle/express/app.js
+++ seattle/express/app.js
@@ -6,8 +6,31 @@ var fib = require('./lib/myFib');
 var lastId= 0;
 var users = [];
 
+myApp.set('views', 'views');
+myApp.set('view engine', 'jade');
+
 myApp.use(express.static('static'));
 myApp.use(bodyParser.urlencoded({ extended: false }));
+
+myApp.use(function randomlyThrowErrors(req, res, next){
+  if(Math.random() > 0.6){
+    var error = new Error('random error!!');
+    error.code = "ERANDOM";
+    next(error);
+  }else{
+    next();
+  }
+});
+
+myApp.use(function randomErrHandler(err, req, res, next){
+  //log error message
+  if(err.code === "ERANDOM"){
+    res.set('X-error-message', 'random error');
+    console.error('random error alert!!');
+  }
+  //continue
+  next();
+});
 
 myApp.get('/', function handleRoot(req, res, next){
   res.send('Hello Werld');
@@ -47,6 +70,17 @@ myApp.get('/users', function getAllUsers(req,res){
   output +='<h2>create another user:</h2>';
   output +='<form method=POST action="/user"><label>Username: <input type=text name=username /></label><button>create user</button></form>';
   res.end(output);
+});
+
+myApp.use(function(req, res, next){
+  res.status(404);
+  // default to plain-text. send()
+  res.type('txt').send('Not found!!');
+  //optionally: sendfile static/404.html
+});
+
+myApp.use(function uncaughtErrorHandler(err, req, res, next){
+  res.type('txt').send(err.message);
 });
 
 myApp.listen(8080);
