f309f13fa2d49b205e24631379a2104ed9148e4b
Sequoia McDowell <sequoia.mcdowell@gmail.com>
2015-07-28 13:59:38 -0700

    get, post user

--- seattle/express/app.js
+++ seattle/express/app.js
@@ -1,11 +1,14 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var myApp = express();
 var fib = require('./lib/myFib');
 
-//Hint : google "static routing middleware"!!
-//http://expressjs.com/guide/using-middleware.html
-//Hint 2: it won't look likle "myApp.get('/static',..."
+var lastId= 0;
+var users = [];
+
 myApp.use(express.static('static'));
+myApp.use(bodyParser.urlencoded({ extended: false }));
+
 myApp.get('/', function handleRoot(req, res, next){
   res.send('Hello Werld');
 });
@@ -14,13 +17,36 @@ myApp.get('/fibonacci/:iterations?', function(req, res){
   var iters = req.params.iterations || 10;
   res.json({iterations: iters, result : fib(iters)});
 });
-//extra credit: allow user to set # iterations in path
-//hint: console.log(req) & inspect it
-//docs: http://expressjs.com/api.html#req
-//myApp.get('/fibonacci/:iterations', function(req, res){});
+
+myApp.get('/user',function userForm(req, res, next){
+  res.send('<form method=POST><label>Username: <input type=text name=username /></label><button>create user</button></form>');
+});
+
+myApp.post('/user', function createUser(req, res, next) {
+  //1. 
+  //add user object to users array
+  //{id : ++lastId, name: 'username'}
+  users.push({
+    id: ++lastId,
+    name: req.body.username
+  });
+  res.redirect('/users');
+});
+
+//2. add route for /users that returns list of all users as id: username
+//(text/plain, text/html)
+myApp.get('/users', function getAllUsers(req,res){
+  var output = '';
+  output +='<h1>Users:</h1>';
+  output +='<table>';
+  output +='<tr><th>id</th><th>name</th></tr>';
+  users.forEach(function(user){
+    output +='<tr><td>'+user.id+'</td><td>'+user.name+'</td></tr>';
+  });
+  output +='</table>';
+  output +='<h2>create another user:</h2>';
+  output +='<form method=POST action="/user"><label>Username: <input type=text name=username /></label><button>create user</button></form>';
+  res.end(output);
+});
 
 myApp.listen(8080);
-
-//expressjs.com
-//  guide
-//  API
