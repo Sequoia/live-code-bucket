74fcf571b4c5ba7d6871852006bb3bffdd3c3054
Sequoia McDowell <sequoia.mcdowell@gmail.com>
2015-07-28 09:49:39 -0700

    static routing

--- seattle/roll-your-own/routes/static.js
+++ seattle/roll-your-own/routes/static.js
@@ -1,15 +1,16 @@
-fs = require('fs');
+var fs = require('fs');
+var path = require('path');
 module.exports = function staticHandler(url, cb){
-  //read file
-  //callback:
-  //  if err cb(err)
-  //  else cb(results: data.toString('utf8')
-
-  //results = {
-    //body : 'you requested + ' + url.pathname
-  //};
-  //process.nextTick(function(){
-    //cb(null, results);
-  //});
+  var filePath = path.join(process.cwd(),url.pathname);
+  console.log(filePath);
+  var options = { 'encoding' : 'utf8' };
+  fs.readFile(filePath, options, function(err, contents){
+    if(err){
+      console.log(err);
+      cb(err); //pass back up to router with error
+      return;
+    }
+    cb(null, {body: contents });
+  });
 };
 
